be24ae5ea6af43534203f148dbcb1783170b2b83
siksik-Choi <80188977+siksik-Choi@users.noreply.github.com>
2021-06-22 04:10:44 +0900

    Update soup.py
    랭킹에 대한 가중치를 포함한 리스트

--- soup.py
+++ soup.py
@@ -1,20 +1,27 @@
+#!/usr/bin/python
+
 from bs4 import BeautifulSoup
 import requests
 import re
 import os
-
+from konlpy.tag import Okt
+from collections import Counter
+ 
+def hfilter(s):
+    return re.sub(u'[^ \u3130-\u318f\uac00-\ud7a3]+','',s)
 
 if __name__ == '__main__':
     try:
-        URL = 'https://www.youtube.com/feed/explore'
-        f = open("data.txt", 'w')
-
+        URL = 'https://www.youtube.co.kr/feed/explore'
+     
         page = requests.get(URL)
         soup = BeautifulSoup(page.content, 'html.parser', from_encoding="utf8")
         search_results = soup.findAll("script")
 
         # script section of the soup
         y = str(search_results)
+
+        my_para = []
 
         # first iteration of the title
         pos = y.find("""title":{"runs":[{"text":""")
@@ -23,7 +30,9 @@ if __name__ == '__main__':
         pos_r = y[pos_x:pos+200]
         sub = """}]"""
         pos_end = pos_x + k.find(sub) - 1
-        #print(y[pos_x:pos_end])
+
+        my_para.append(y[pos_x:pos_end])
+         #print(y[pos_x:pos_end])
 
         # repetition
         i = 1
@@ -35,32 +44,47 @@ if __name__ == '__main__':
             pos_r = y[pos_x:pos+200]
             sub = """}]"""
             pos_end = pos_x + k.find(sub) - 1
-            f.write(y[pos_x:pos_end])
+
+            my_para.append(y[pos_x:pos_end])
+            
+           
             i += 1
-        f.close()
 
-        file = open("data.txt", 'r')
-        delete = re.sub("""!@#$%^&*()_-+={[}]|\;:"‘'·<>?/., """, "", file.read())
-        read = delete.split()
-        relen = len(read)
-        print("단어 수: %d" % relen)
-        Di = {}
-        for i in range(relen):
-            plus = 0
-            for j in range(relen):
-                if read[i] == read[j]:
-                        plus += 1
-            Di[read[i]] = plus
-        As = sorted(Di.items(), key=lambda x: x[1], reverse=True)
-
-        Dil = len(Di)
-
-        for g in range(Dil):
-           print("\'%s\'의 개수 : %s" % (As[g][0], As[g][1]))
-
+        print("---------------------------------------------")
+        print(my_para)
+        print("---------------------------------------------")
+       
         
-        f.close()
+        okt = Okt()
+        word_d = {}
+        words = []
+        weight = 60
 
+        for para in my_para:
+            hsent = hfilter(para)
+            print(hsent)
+
+            noun = okt.nouns(hsent)
+            print(noun)
+
+            for n in noun:
+                cnt = 0
+                if(len(n)>1):
+                    while cnt < weight//10:
+                        words.append(n)
+                        cnt = cnt +1
+            
+            weight = weight-1
+        
+        count = Counter(words)
+
+        noun_list = count.most_common(100)
+        for v in noun_list:
+                print(v)
+        
+
+        ("---------------------------------------------")
+        
 
 
     except BaseException:
